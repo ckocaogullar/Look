c7ff965fe57b7d5a98087f3800c3706677344ce7
GizemAyas517 <gayas14@ku.edu.tr>
2017-07-31 19:44:44 +0300

    profile screen added but empty

---
+++ src/Components/MatrixList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { View, Image, FlatList} from 'react-native';
+
+class MatrixList extends Component{
+
+
+    render(){
+        return(
+            <View>
+
+            </View>
+        );
+    }
+
+
+
+}
+
+module.exports = MatrixList;
+
